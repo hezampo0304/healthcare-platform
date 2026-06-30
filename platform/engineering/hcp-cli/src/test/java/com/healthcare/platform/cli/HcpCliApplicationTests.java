a761366206f6718d06c9cba174a52f060b9ca8a0
Henry Zambrano Polentino <hzambra@bancolombia.com.co>
2026-06-30 13:43:28 -0500

    no message

---
+++ platform/engineering/hcp-cli/src/test/java/com/healthcare/platform/cli/HcpCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.platform.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcpCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
